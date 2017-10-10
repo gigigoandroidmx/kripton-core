e480e82b90bfe45cc452154d78e7e86b0e5e46ba
Omar Bacilio <omar.bacilio@gigigo.com.mx>
2017-10-10 14:19:45 -0500

    Método para obtener versión de aplicación

--- kbase/src/main/java/com/gigigo/kbase/presentation/utils/General.java
+++ kbase/src/main/java/com/gigigo/kbase/presentation/utils/General.java
@@ -1,6 +1,8 @@
 package com.gigigo.kbase.presentation.utils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -12,5 +14,18 @@ public class General {
     public static void hideKeyboard(Context context, View input) {
         InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(input.getWindowToken(), 0);
+    }
+
+    public static String getVersionName(Context context) {
+        try {
+            PackageInfo packageInfo =
+                    context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
